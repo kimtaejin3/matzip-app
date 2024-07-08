7a0d059a90bb6f5af32c69f05123eb64bf9106c5
kimtaejin3 <rlaxowls1316@naver.com>
2024-07-08 20:04:30 +0900

    [3-2] Input 공통 컴포넌트 구현하기

--- frontend/src/constants/colors.ts
+++ frontend/src/constants/colors.ts
@@ -2,6 +2,12 @@ const colors = {
   WHITE: '#fff',
   PINK_500: '#BF5C79',
   PINK_700: '#C63B64',
+  RED_300: '#ffb4b4',
+  RED_500: '#ff5f5f',
+  GRAY_200: '#e7e7e7',
+  GRAY_500: '#8e8e8e',
+  GRAY_700: '#575757',
+  BLACK: '#000',
 };
 
 export {colors};
