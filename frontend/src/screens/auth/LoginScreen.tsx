2066148f92ac80a07308d22cacb2a823c7a40116
kimtaejin3 <rlaxowls1316@naver.com>
2024-07-09 15:06:54 +0900

    [3-5] 폼 사용성 개선하기

--- frontend/src/screens/auth/LoginScreen.tsx
+++ frontend/src/screens/auth/LoginScreen.tsx
@@ -1,11 +1,13 @@
-import React, {useState} from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {SafeAreaView, StyleSheet, Text, TextInput, View} from 'react-native';
 import InputField from '../../components/InputField';
 import CustomButton from '../../components/CustomButton';
 import useForm from '../../hooks/useForm';
-import {validateLogin} from '../../utils/validate';
+import {validateLogin} from '../../utils';
 
 function LoginScreen() {
+  const passwordRef = useRef<TextInput | null>(null);
+
   const login = useForm({
     initialValue: {email: '', password: ''},
     validate: validateLogin,
@@ -19,17 +21,26 @@ function LoginScreen() {
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
         <InputField
+          autoFocus
           placeholder="이메일"
           touched={login.touched?.email}
           inputMode="email"
           error={login.errors?.email}
+          returnKeyType="next"
+          blurOnSubmit={false}
+          onSubmitEditing={() => passwordRef.current?.focus()}
           {...login.getTextInputProps('email')}
         />
         <InputField
+          ref={passwordRef}
           placeholder="비밀번호"
           touched={login.touched?.password}
           secureTextEntry
           error={login.errors?.password}
+          returnKeyType="join"
+          blurOnSubmit={false}
+          textContentType="oneTimeCode"
+          onSubmitEditing={handleSubmit}
           {...login.getTextInputProps('password')}
         />
       </View>
--- frontend/src/screens/auth/SignUpScreen.tsx
+++ frontend/src/screens/auth/SignUpScreen.tsx
@@ -1,43 +1,64 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React, {useRef} from 'react';
+import {SafeAreaView, StyleSheet, Text, TextInput, View} from 'react-native';
 import InputField from '../../components/InputField';
 import useForm from '../../hooks/useForm';
-import {validateSignup} from '../../utils/validate';
+import {validateSignup} from '../../utils';
 import CustomButton from '../../components/CustomButton';
 
 interface SignUpScreenProps {}
 
 function SignUpScreen({}: SignUpScreenProps) {
+  const passwordRef = useRef<TextInput | null>(null);
+  const passwordConfirmRef = useRef<TextInput | null>(null);
+
   const signup = useForm({
     initialValue: {email: '', password: '', passwordConfirm: ''},
     validate: validateSignup,
   });
 
+  const handleSubmit = () => {
+    console.log(signup.values);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
         <InputField
+          autoFocus
           placeholder="이메일"
           touched={signup.touched?.email}
           inputMode="email"
           error={signup.errors?.email}
+          returnKeyType="next"
+          blurOnSubmit={false}
+          onSubmitEditing={() => passwordRef.current?.focus()}
           {...signup.getTextInputProps('email')}
         />
         <InputField
+          ref={passwordRef}
           placeholder="비밀번호"
           touched={signup.touched?.password}
           secureTextEntry
           error={signup.errors?.password}
+          returnKeyType="next"
+          blurOnSubmit={false}
+          onSubmitEditing={() => passwordConfirmRef.current?.focus()}
+          textContentType="oneTimeCode"
           {...signup.getTextInputProps('password')}
         />
         <InputField
+          ref={passwordConfirmRef}
           placeholder="비밀번호 확인"
           touched={signup.touched?.passwordConfirm}
           secureTextEntry
           error={signup.errors?.passwordConfirm}
+          returnKeyType="join"
+          blurOnSubmit={false}
+          textContentType="oneTimeCode"
+          onSubmitEditing={handleSubmit}
           {...signup.getTextInputProps('passwordConfirm')}
         />
-        <CustomButton label="회원가입" />
+        <CustomButton label="회원가입" onPress={handleSubmit} />
       </View>
     </SafeAreaView>
   );
